2341a2f517f65abfd9dc283f8649705e603aed1d
KemoLD <kemo.sonko1@gmail.com>
2024-05-02 17:31:51 -0700

    added first files

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"gin-gonic/gin"
+	"net/http"
+)
+
+type Book struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Author string `json:"author"`
+	Quality int `json:"quality"`
+}
+
+var books = []Book{
+	Book{ID: "1", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quality: 5},
+	Book{ID: "2", Title: "To Kill a Mockingbird", Author: "Harper Lee", Quality: 4},
+	Book{ID: "3", Title: "1984", Author: "George Orwell", Quality: 3},
+}
+
+func getBooks(c *gin.Context) {
+	c.JSON(http.StatusOK, books)
+}
+
+func main(){
+	r := gin.Default()
+	r.GET("/books", getBooks)
+	r.Run(":8080")
+}
